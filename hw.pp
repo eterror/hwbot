aa1f8f7e66de5735b953b601f661d865233ad886
eterror <solargrim@gmail.com>
2014-01-03 01:00:37 +0100

    kick disabled for bot admin

--- hw.pp
+++ hw.pp
@@ -463,6 +463,13 @@ begin
 	if (copy(s2, 1, 5) = '.kick') and (s1 = HW_ADMIN) then
 	begin
 	    s3:=ExtractWord(2, s2, [#32]);
+	    
+	    if (s3 = HW_ADMIN) then
+	    begin
+		writeln(sout, 'CHAT',#10,s1,': No way sir!',#10);
+		exit;
+	    end;
+	    
 	    writeln('[*] Kicking user from room: ',s3);
 	    writeln(sout, 'KICK',#10,s3,#10);
 	    exit;
